da45500a8f1fa7e93d7b157d2501f3becd28fd75
msusaes <norton18@msu.edu>
2023-12-03 17:44:26 -0500

    adding catch statement for non MRIs

--- pages/2_🧠_Classify.py
+++ pages/2_🧠_Classify.py
@@ -15,13 +15,12 @@ st.set_page_config(page_title='Classify - NeuroNet MRI',
                     layout="wide")
 st.title("Classify - NeuroNet MRI")
 
-file = st.file_uploader("Please upload an MRI scan: ", type=["jpg", "png", "jpeg"])
+file = st.file_uploader("Please upload an MRI scan: ", type=["jpg", "jpeg"])
 url = "https://drive.google.com/drive/folders/1hBKM8E6SPLeYVhU7wuAIWDhhfd5EiEAn?usp=sharing"
 st.write("~ Don't have a image to upload? Try one of [these](%s) ~" % url)
 st.text ("[files displayed come from an un-tested and un-trained set of data to be used for exploration]")
 
 if file is not None:
-
     st.subheader("Uploaded Image Preview:")
     image = Image.open(file)
     st.image(image, width=500)
@@ -37,15 +36,17 @@ if file is not None:
         CNN_model = tf.keras.models.load_model('assets/MRI_CNN.h5')
         prediction = CNN_model.predict(image_batch)
         p = CNN_model.predict_generator(image_batch)
-        st.write(p)
-        y_class = np.argmax(prediction,axis=1)
+        p = max(list(p[0]))
+        if p < 0.95:
+            st.error("An error has occurred. Are you sure you uploaded an MRI scan? (reminder: the model can only handle a single scan at a time)", icon="🚨")
+        else:
+            y_class = np.argmax(prediction,axis=1)
 
 
-        dict = {0 : 'glioma tumor', 1 : 'no tumor', 2: 'meningioma tumor', 3 : 'pituitary tumor'}
+            dict = {0 : 'glioma tumor', 1 : 'no tumor', 2: 'meningioma tumor', 3 : 'pituitary tumor'}
 
-        y_class = list(map(dict.get, y_class, y_class))[0]
+            y_class = list(map(dict.get, y_class, y_class))[0]
 
 
-        st.markdown("#### Predicted Class: "+ y_class)
-        st.write("The untrained eye cannot quickly identify the presence or classification of neuro tumors, however, the CNN developed in NeuroNet MRI has the ability to detect and classify new images with high accuracy.")
-
+            st.markdown("#### Predicted Class: "+ y_class)
+            st.write("The untrained eye cannot quickly identify the presence or classification of neuro tumors, however, the CNN developed in NeuroNet MRI has the ability to detect and classify new images with high accuracy.")
